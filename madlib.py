d6bba9b504394f977f3926d18f9f4cbdfc32d9be
EvilGenius <93214149+EvilGenius13@users.noreply.github.com>
2022-08-29 20:27:49 -0400

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,62 @@
+
+# Homework: Create a madlib. Imagine a story where some of the words are 
+# supplied by user input. Using python you will use input to collect 
+# words for a story and then display the story. 
+
+# Use input to collect each word to a variable
+# Use an f string to display the story
+
+# Your madlib must collect at least 6 words!
+
+adjective_one = input("Please write an adjective: ")
+noun_one = input("Please write a noun: ")
+verb_past_tense = input("Please write verb in the past tense: ")
+adverb_one = input("Please write an adverb: ")
+adjective_two = input("Please write an adjective: ")
+noun_two = input("Please write a noun: ")
+noun_three = input("Pleas write a noun: ")
+adjective_three = input("Please write an adjective: ")
+verb_one = input("Please write a verb: ")
+adverb_two = input("Please write an adverb: ")
+verb_past_tense_2 = input("Please write a verb in the past tense: ")
+adjective_four = input("Please write an adjective: ")
+
+
+
+
+# --------------------------------------------------
+# Partial solution
+print(f"Today I went to the zoo. I saw an {adjective_one}")
+print(f"{noun_one} jumping up and down in it's tree")
+print(f"He {verb_past_tense} {adverb_one}")
+print(f"through the large tunnel that led to its {adjective_two}")
+print(f"{noun_two}. I got some peanuts and passed")
+print(f"them through the cage to a gigantic gray {noun_three}")
+print(f"towering above my head. Feeding that animal made")
+print(f"me hungry. I went to get a {adjective_three} scoop")
+print(f"of ice cream. It filled my stomach. Afterwards I had to")
+print(f"{verb_one} {adverb_two} to catch our bus")
+print(f"When I got home I {verb_past_tense_2} my")
+print(f"mom for a {adjective_four} day at the zoo")
+
+
+
+
+# name = input("Name:")
+# color = input("color:")
+# num = input("Number:")
+
+# print(f"{name} wore {color} shoes while they counted to {num}")c
+
+# print(f"Twas the night before Channukah when all through the house")
+# print(f"not a creature was stirring, not even a {noun_one}")
+# print(f"The Menorah was lit by the table with care")
+# print(f"in hopes that {noun_two} would be there")
+# print(f"The children were {verb_past_tense} all snug in their beds")
+# print(f"while visions of {noun_three} danced in their heads")
+# print(f"When out on the roof there arose such a {verb_one}")
+# print(f"I {adjective_one} from my bed to see what was the matter")
+# print(f"when what to my {adjective_two} eyes should appear")
+# print(f"but a miniature Goliath holding a souvenir")
+# print(f"not to worry though, he was just delivering a new dreidel")
+# print(f"The End")
